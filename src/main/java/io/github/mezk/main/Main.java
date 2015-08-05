cf60e0ae61d0298e3e713a74f758d1698b06e86e
Andrei Selkin <andreyselkin@gmail.com>
2015-08-05 23:46:20 +0300

    Updated Main.

--- src/main/java/io/github/mezk/main/Main.java
+++ src/main/java/io/github/mezk/main/Main.java
@@ -1,24 +1,48 @@
 package io.github.mezk.main;
 
+import javax.servlet.Servlet;
+
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
-import io.github.mezk.frontend.Frontend;
+import io.github.mezk.frontend.SignInServlet;
+import io.github.mezk.frontend.SignUpServlet;
 
 /**
  * @author Andrei Selkin
  */
 public class Main {
     public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args.length == 1) {
+            String portString = args[0];
+            port = Integer.valueOf(portString);
+        }
 
-        Frontend frontend = new Frontend();
+        System.out.append("Starting at port: ").append(String.valueOf(port)).append('\n');
+
+        AccountService accountService = new AccountService();
+
+        Servlet signin = new SignInServlet(accountService);
+        Servlet signUp = new SignUpServlet(accountService);
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(frontend), "/authform");
+        context.addServlet(new ServletHolder(signin), "/signin");
+        context.addServlet(new ServletHolder(signUp), "/signup");
 
-        Server server = new Server(8080);
-        server.setHandler(context);
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(true);
+        resource_handler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
+
+        Server server = new Server(port);
+        server.setHandler(handlers);
 
         server.start();
         server.join();
